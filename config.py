5badddb04fa725896272fb2e0915536e072d445c
Xiaotan Sun <93351260+sxtforreal@users.noreply.github.com>
2025-08-06 15:27:20 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,33 @@
+import torch
+
+# Set device cuda for GPU if it is available, otherwise run on the CPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Training parameters
+min_epochs = 1
+max_epochs = 2
+input_dim = 768
+reduced_dim = 256
+latent_dim = 256
+levels = 2
+heads = 2
+m = 16
+beta = 0.5
+lr = 1e-4
+num_classes = 2
+ff_dim = 256
+transformer_heads = 2
+transformer_layers = 2
+pretrained_ckpt = "/home/sunx/data/aiiih/projects/sunx/projects/TEEMR/PT/outputs/checkpoints/best_SetVAE.ckpt"
+w = 0.5
+free_bits = 0.2
+
+# Logger
+name = "SeqSetVAE-v2"
+log_every_n_steps = 200
+ckpt_every_n_steps = 200
+
+# Compute related
+accelerator = "gpu"
+devices = 1
+precision = "16-mixed"
